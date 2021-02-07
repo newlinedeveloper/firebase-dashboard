78ea237f923ca08c2ee0c2b86b83b925ea322e21
sathish <sathishc195@gmail.com>
2021-02-07 17:54:13 +0530

    users and post page

---
+++ AdminLTE-3.0.5/firebaseconfig.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.5/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBDsVeg9pm5-XZbwzFe1P3dn5aCLyQKtAc",
+    authDomain: "ciboh-afe34.firebaseapp.com",
+    projectId: "ciboh-afe34",
+    storageBucket: "ciboh-afe34.appspot.com",
+    messagingSenderId: "940229906316",
+    appId: "1:940229906316:web:537c2622e85955da3890a9",
+    measurementId: "G-7DSJQ2Q4B4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
